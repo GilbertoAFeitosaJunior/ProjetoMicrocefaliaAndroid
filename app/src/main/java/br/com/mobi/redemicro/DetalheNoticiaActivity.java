88ec69dc91683f4582ffccc81d751b3ea01f958b
Allesson <allessoncavalcante@gmail.com>
2016-11-22 20:24:47 -0300

    exibir detalhe de noticias

--- app/src/main/java/br/com/mobi/redemicro/DetalheNoticiaActivity.java
+++ app/src/main/java/br/com/mobi/redemicro/DetalheNoticiaActivity.java
@@ -1,44 +1,25 @@
 package br.com.mobi.redemicro;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
-import android.util.Log;
-import android.view.ContextMenu;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.MalformedURLException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import br.com.mobi.redemicro.bean.Comentar;
@@ -48,8 +29,6 @@ import br.com.mobi.redemicro.bo.NoticiaBo;
 import br.com.mobi.redemicro.bo.UsuarioBo;
 import br.com.mobi.redemicro.util.Constants;
 import br.com.mobi.redemicro.util.HttpAsyncTask;
-import mobi.stos.podataka_lib.exception.NoPrimaryKeyFoundException;
-import mobi.stos.podataka_lib.exception.NoPrimaryKeyValueFoundException;
 
 public class DetalheNoticiaActivity extends AppCompatActivity {
 
@@ -79,8 +58,6 @@ public class DetalheNoticiaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detalhe_noticia);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //listaDeCurtir = new ArrayList<>();
         //curtirBO = new CurtirBO(this);
@@ -131,7 +108,8 @@ public class DetalheNoticiaActivity extends AppCompatActivity {
         }
     }
 
-    public class DetalhesTask extends AsyncTask<Void, Void, Void> {
+    public class DetalhesTask extends AsyncTask<Void, Void, Boolean> {
+        private boolean opc;
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -143,8 +121,7 @@ public class DetalheNoticiaActivity extends AppCompatActivity {
         }
 
         @Override
-        protected Void doInBackground(Void... params) {
-
+        protected Boolean doInBackground(Void... params) {
             try {
                 SharedPreferences preferences = getSharedPreferences(Constants.APP, MODE_PRIVATE);
                 int noticiaId = preferences.getInt("NOTICIA", 0);
@@ -168,9 +145,12 @@ public class DetalheNoticiaActivity extends AppCompatActivity {
                                 noticia.setNoticia(json.getString("noticia"));
 
 
-                                noticiaBO.update(noticia);
-                            } catch (JSONException | NoPrimaryKeyValueFoundException | NoPrimaryKeyFoundException e) {
+                                noticiaBO.clean();
+                                noticiaBO.insert(noticia);
+                                opc=true;
+                            } catch (Exception e) {
                                 e.printStackTrace();
+                                opc=false;
                             }
 
 
@@ -183,25 +163,49 @@ public class DetalheNoticiaActivity extends AppCompatActivity {
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
-            return null;
+            return opc;
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
+        protected void onPostExecute(Boolean aVoid) {
+            super.onPostExecute(aVoid);
 
-            Picasso.with(DetalheNoticiaActivity.this).load(noticia.getFoto()).placeholder(android.R.drawable.ic_menu_camera).into(imagemDetalhesNoticia);
-            tituloDetalhes.setText(noticia.getTitulo());
+            if (aVoid) {
+                Picasso.with(DetalheNoticiaActivity.this).load(noticia.getFoto()).placeholder(android.R.drawable.ic_menu_camera).into(imagemDetalhesNoticia);
+                tituloDetalhes.setText(noticia.getTitulo());
 
-            String dataPublicacao = new SimpleDateFormat(getString(R.string.data_hora_format)).format(noticia.getData());
-            dataPublicacaoDetalhes.setText("Publicado em: " + dataPublicacao);
-            conteudoDetalhes.setText(Html.fromHtml(noticia.getNoticia()).toString());
+                //String dataPublicacao = new SimpleDateFormat(getString(R.string.data_hora_format)).format(noticia.getData());
+                dataPublicacaoDetalhes.setText("Publicado em: " + noticia.getData());
+                conteudoDetalhes.setText(Html.fromHtml(noticia.getNoticia()).toString());
 
-            if (progressDialog != null && progressDialog.isShowing()) {
-                progressDialog.dismiss();
+                if (progressDialog != null && progressDialog.isShowing()) {
+                    progressDialog.dismiss();
+                }
+            } else {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        falhaNaNotícia();
+                    }
+                });
             }
 
-            super.onPostExecute(aVoid);
+
+
         }
+    }
+    public void falhaNaNotícia() {
+        android.app.AlertDialog alertDialog = new android.app.AlertDialog.Builder(this).create();
+        alertDialog.setTitle(getString(R.string.title_login_erro));
+        alertDialog.setMessage(getString(R.string.title_activity_aqwq));
+        alertDialog.setButton(android.app.AlertDialog.BUTTON_POSITIVE, getString(R.string.ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                DetalheNoticiaActivity.this.finish();
+            }
+        });
+        alertDialog.show();
     }
 
 }
+
